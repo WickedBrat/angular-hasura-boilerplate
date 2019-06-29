b0594a5e282664809124720280d798a98f94078b
Dave A-W <dave.aw+git@gmail.com>
2019-06-29 08:55:39 +0800

    Remove Hasura access key

--- advanced/src/environments/environment.ts
+++ advanced/src/environments/environment.ts
@@ -13,7 +13,6 @@ export const environment = {
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US'],
   graphqlEndpoint: '<replace-with-your-endpoint>',
-  hasuraAccessKey: '<your-hasura-endpoint-access-key>',
   auth0ClientId: '<client-id>',
   auth0Domain: '<auth-domain>',
   auth0RedirectUri: '<redirect-uri>'
