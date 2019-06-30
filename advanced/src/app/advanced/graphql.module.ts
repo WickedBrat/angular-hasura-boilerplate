25695e870a2db2debc401aa390de5585985062f7
Dave A-W <David.Allen-Williams@decipher.com.au>
2019-06-30 11:53:31 +0800

    Improve auth logout and localStorage usage

--- advanced/src/app/advanced/graphql.module.ts
+++ advanced/src/app/advanced/graphql.module.ts
@@ -5,25 +5,31 @@ import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { environment } from '@env/environment.prod';
 import { ApolloLink } from 'apollo-link';
+import { AuthenticationService } from '@app/core';
 
 @NgModule({
   exports: [HttpClientModule, ApolloModule, HttpLinkModule]
 })
 export class GraphQLModule {
-  constructor(apollo: Apollo, httpLink: HttpLink) {
-
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink,
+    authenticationService: AuthenticationService
+  ) {
     // Create a link to the graphQL endpoint
     const http = httpLink.create({ uri: environment.graphqlEndpoint });
 
     // Establish middleware to add the authentication token from local storage
     const middleware = new ApolloLink((operation, forward) => {
-      const token = localStorage.getItem('id_token');
-      if (token) {
+      if (authenticationService.isAuthenticated) {
         // NOTE: For Auth0, the token claims are read to get the X-Hasura-User-Id and role information.
         // This info is injected using a `hasura-jwt-claim` rule established in Auth0, as described at
         // https://auth0.com/blog/building-a-collaborative-todo-app-with-realtime-graphql-using-hasura/#Authentication-Using-Auth0
         operation.setContext({
-          headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
+          headers: new HttpHeaders().set(
+            'Authorization',
+            `Bearer ${authenticationService.idToken}`
+          )
         });
       }
       return forward(operation);
